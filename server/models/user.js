616db1df6bd40ab5dfdcce24b10303e07b122af5
s221061447 <s221061447@deakin.edu.au>
2022-04-28 05:31:09 +1000

    Changed export of user model to user functions, implemented isActive based on role, and updated Models and Flows document to include isActive field

--- server/models/user.js
+++ server/models/user.js
@@ -7,9 +7,37 @@ const userSchema = new mongoose.Schema({
     email: { type: String, unique: true },
     password: { type: String },
     role: { type: String },
-    token: { type: String }
+    token: { type: String },
+	is_active: { type: Boolean }
 }, {
   versionKey: false
 });
 
-module.exports = mongoose.model("users", userSchema);
+const userModel = mongoose.model("users", userSchema);
+
+const createUser = async (id, firstName, lastName, email, password, role, token, isActive) => {
+	return userModel.create({
+		_id: id,
+		first_name: firstName,
+		last_name: lastName,
+		email: email,
+		password: password,
+		role: role,
+		token: token,
+		is_active: isActive
+	});
+};
+
+const doesUserExist = async (id) => {
+	return userModel.exists({ _id: id });
+};
+
+const getUser = async (id) => {
+	return userModel.findById(id);
+};
+
+const updateUser = async (id, updateJson) => {
+	return userModel.findByIdAndUpdate(id, updateJson, { new: true });
+};
+
+module.exports = { createUser, doesUserExist, getUser, updateUser };
