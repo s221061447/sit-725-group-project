ea16d82a8eb0c3239b596f5ea6efa68cd8fa6f52
swathireddy1862 <swathi.reddy1862@gmail.com>
2022-05-15 21:25:01 +1000

    execution through app.js implemented

--- server/app.js
+++ server/app.js
@@ -2,11 +2,28 @@ const express = require("express");
 const authApi = require("./controllers/auth.js");
 const { authorize } = require('./middleware/jwt');
 const roles = require("./util/roles");
-var cors = require('cors')
+var cors = require('cors');
+var path = require('path');
+const expressSession = require("express-session");
+const connectMongo = require("connect-mongo");
+const { MONGODB_URI } = process.env;
+
+// const loginUserController = require("./controllers/loginUser");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded());
 app.use(cors());
+
+app.use(
+    expressSession({
+        secret: "secret",
+        store: connectMongo.create({
+            mongoUrl: MONGODB_URI
+        })
+    })
+);
+
 
 // Configure routes
 app.use('/auth', authApi);
@@ -15,6 +32,27 @@ app.use('/auth', authApi);
 app.get("/welcome", authorize(roles.User), (req, res) => {
     res.status(200).send("Welcome 🙌");
 });
+
+app.post("/users/login", loginUserController);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/register',(req,res)=>{
+    res.sendFile(__dirname+'/UI/register.html');
+})
+
+app.use('/login',(req,res)=>{
+    res.sendFile(__dirname+'/UI/login.html');
+})
+
+app.use('/dashboard',(req,res)=>{
+    res.sendFile(__dirname+'/UI/dashboard.html');
+})
+
+app.use('/forgotPassword',(req,res)=>{
+    res.sendFile(__dirname+'/UI/forgot-password.html');
+})
+
 
 // This should be the last route. Any after it won't work.
 app.use("*", (req, res) => {
